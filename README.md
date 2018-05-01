27adabdbd5e1dc433584f917f5e297a7e036638c
Ilya Sakharchuk <saharchuk999@gmail.com>
2018-05-02 01:03:09 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# VK.com song lyrics buttons
+Add search lyrics buttons for songs in vk
+
+
+## Installation
+You will need a browser extension for loading userscripts. The reccommended ones are [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) for Firefox, and [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) for Chrome.
+
+To install the userscript, simply click the link below, or go to the ".user.js" files in the repository and click the "Raw" button in the top right. Your userscript extension should recognize the script and ask if you would like to install it.
+
+Link to install:
+[vk-song-lyrics.user.js](/vk-song-lyrics.user.js?raw=true)
---
+++ vk-song-lyrics.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Vk song lyrics
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Add search lyrics buttons for songs in vk
+// @author       @Vanawy [Vanawy Firo]
+// @match        https://vk.com/*
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    var i = 0;
+    $(document).on("mouseenter",".audio_row",function() {
+        //audio_row__title_inner - title
+        //audio_row__performer   - artist
+        var attr = $(this).parent().attr('id');
+
+        // For some browsers, `attr` is undefined; for others,
+        // `attr` is false.  Check for both.
+        if (typeof attr === typeof undefined || attr === false) {
+            $( this ).wrap( "<div class='song' id='song"+i+"'></div>" );
+            var artist = $("#song"+i).find('.audio_row__performer').text();
+            var title = $("#song"+i).find('.audio_row__title_inner').text();
+            $("#song"+i)
+                .append("<div style='' id='sl_song"+i+"'><a target='_blank' href='https://genius.com/search?q="+encodeURI(artist+" - "+title)+"'> [Genius Lyrics]</a><a target='_blank' href='https://www.google.com/search?q="+encodeURI(artist+" - "+name+" lyrics")+"'> [Search Lyrics]</a><a target='_blank' href='https://www.google.com/search?q="+encodeURI(artist+" - "+name+" перевод")+"'> [Перевод]</a></div>");
+            i++;
+        }else{
+            $("#sl_"+attr).css("display", "contents");
+        }
+    });
+    $(document).on("mouseleave",".song",function() {
+        var attr = $(this).attr('id');
+        $("#sl_"+attr).css("display", "none");
+    });
+    $(document).on("mouseenter",".song",function() {
+        var attr = $(this).attr('id');
+        $("#sl_"+attr).css("display", "contents");
+    });
+})();
