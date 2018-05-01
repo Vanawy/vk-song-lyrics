bbeb7afc2d0cb5a23a385b2fc3eb0e49cc5e1a0a
Ilya Sakharchuk <saharchuk999@gmail.com>
2018-05-02 01:19:20 +0300

    Update vk-song-lyrics.user.js

--- vk-song-lyrics.user.js
+++ vk-song-lyrics.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vk song lyrics
 // @namespace    vanawy
-// @version      1.2
+// @version      1.0.3
 // @description  Add search lyrics buttons for songs in vk
 // @author       @Vanawy [Vanawy Firo]
 // @match        https://vk.com/*
@@ -23,7 +23,11 @@
             var artist = $("#song"+i).find('.audio_row__performer').text();
             var title = $("#song"+i).find('.audio_row__title_inner').text();
             $("#song"+i)
-                .append("<div style='' id='sl_song"+i+"'><a target='_blank' href='https://genius.com/search?q="+encodeURI(artist+" - "+title)+"'> [Genius Lyrics]</a><a target='_blank' href='https://www.google.com/search?q="+encodeURI(artist+" - "+name+" lyrics")+"'> [Search Lyrics]</a><a target='_blank' href='https://www.google.com/search?q="+encodeURI(artist+" - "+name+" перевод")+"'> [Перевод]</a></div>");
+                .append("<div style='' id='sl_song"+i+"'>
+                        <a target='_blank' href='https://genius.com/search?q="+encodeURI(artist+" - "+title)+"'> [Genius Lyrics]</a>
+                        <a target='_blank' href='https://www.google.com/search?q="+encodeURI(artist+" - "+name+" lyrics")+"'> [Search Lyrics]</a>
+                        <a target='_blank' href='https://www.google.com/search?q="+encodeURI(artist+" - "+name+" перевод")+"'> [Перевод]</a>
+                        </div>");
             i++;
         }else{
             $("#sl_"+attr).css("display", "contents");
